92ae6358a9920e26d55d9d5021d94d48cf4cf870
kwon <kwon@unet.kr>
2021-11-11 08:54:01 +0900

    초기화 때문에 빈 배열로 저장되었던거 수정.

--- rule_count_resort/resort_rules_by_count.js
+++ rule_count_resort/resort_rules_by_count.js
@@ -3,19 +3,22 @@ const resortRulesByHitCount = (arr) => {
   const targetArr = [];
   for (let i = 0; i < arr.length; i++) {
     targetArr.push(arr[i]);
-    if (arr[i].action === 'deny' || i === arr.length - 1) {
-      ret.push(sortUpByManyHitCount(targetArr));
+    if (arr[i].action === "deny" || i === arr.length - 1) {
+      ret.push(...sortUpByManyHitCount(targetArr));
       targetArr.length = 0;
     }
   }
   return ret;
 };
 
-const sortUpByManyHitCount = (rules) => {
+const sortUpByManyHitCount = (arr) => {
+  const rules = [...arr];
   for (let i = 0; i < rules.length; i++) {
-    if (rules[i].action === 'allow') {
+    if (rules[i].action === "allow") {
+      // Deny 변경 X
       for (let j = 0; j < rules.length; j++) {
-        if (rules[j].action !== 'deny' && rules[i].count > rules[j].count) {
+        if (rules[j].action !== "deny" && rules[i].count > rules[j].count) {
+          // Deny 변경 X
           const temp = rules[i];
           rules[i] = rules[j];
           rules[j] = temp;
@@ -24,9 +27,10 @@ const sortUpByManyHitCount = (rules) => {
     }
   }
   return rules;
-}
+};
 
 const result = resortRulesByHitCount(RULES);
 console.log(result);
-document.getElementById('result').innerHTML
-  = result.map(rule => JSON.stringify(rule)).join('<br>');
+document.getElementById("result").innerHTML = result
+  .map((rule) => JSON.stringify(rule))
+  .join("<br>");
